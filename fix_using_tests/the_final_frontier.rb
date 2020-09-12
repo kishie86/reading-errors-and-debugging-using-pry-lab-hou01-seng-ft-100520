1bb2490523c7576ac0cbd7ca97c1951a0d27a98d
Kishana Liburd <kishana.liburd@gmail.com>
2020-09-12 04:11:07 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,4 +1,4 @@
-require 'pry'
+`require 'pry'
 
 def generate_star_date
   (rand(100000) + 400000) / 10.0
@@ -13,4 +13,4 @@ end
 def engage
   date = generate_star_date
   puts state_log(date)
-end
+end`
